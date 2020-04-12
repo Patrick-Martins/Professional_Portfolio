4bec9e917285a27228ee3ddbd83cdbe8532bba8e
Patrick <patr983e@stud.kea.dk>
2020-04-13 00:10:01 +0200

    made each page scrollable to snap to center

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function init() {
   const animPage2 = new gsap.timeline();
   animPage2.add(gsap.from("#myName", 1, { opacity: 0 }));
   animPage2.add(gsap.from("#introText", 2, { opacity: 0, delay: 0.5, rotateX: 90, transformOrigin: "50% 50% -200px" }));
+  gsap.to("#portrait_image", 2, { backgroundSize: "100%", ease: "steps(12)", yoyo: true, repeat: -1, repeatDelay: 2 });
 }
 
 async function fetchArrowSVG() {
@@ -19,12 +20,20 @@ async function fetchArrowSVG() {
   console.log(mySVGData);
   console.log("hey");
 
-  document.querySelector(".arrowDown").innerHTML += mySVGData;
+  document.querySelector("#arrowDown").innerHTML += mySVGData;
 
   animateArrow();
   //   document.getElementById("room").setAttribute("viewBox", "270 120 100 100");
 }
 
 function animateArrow() {
-  gsap.from(".arrowDown svg #arrowDown", 2, { opacity: 0, scaleY: 0, transformOrigin: "bottom", ease: Power1.linear, yoyo: true, repeat: -1 });
+  gsap.from("#arrowDown svg #arrowDown", 2, { opacity: 0, scaleY: 0, transformOrigin: "bottom", ease: Power1.linear, yoyo: true, repeat: -1 });
+}
+
+document.querySelector("#arrowDown").addEventListener("click", goPage2);
+// window.addEventListener("scroll", goPage2);
+
+function goPage2() {
+  window.location.href = "#secondPage";
+  console.log("scroll");
 }
